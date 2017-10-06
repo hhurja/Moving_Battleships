76d3c4c16c676bb3f4f8d7e01b7a735daf5bc7d0
Shabina Rayan <srayan@usc.edu>
2017-10-06 14:27:03 -0700

    the schedule and timerange classes for the model

--- Focus/app/src/main/java/Model/TimeRange.java
+++ Focus/app/src/main/java/Model/TimeRange.java
@@ -1,8 +1,66 @@
 package Model;
-
 /**
  * Created by aaronrschrock on 10/6/17.
  */
 
+import java.sql.Time;
+import java.util.ArrayList;
+
 public class TimeRange {
+
+    private Time startTime;
+    private Time endTime;
+    private boolean repeat;
+    private boolean sunday;
+    private boolean monday;
+    private boolean tuesday;
+    private boolean wednesday;
+    private boolean thursday;
+    private boolean friday;
+    private boolean saturday;
+
+    public TimeRange(Time st, Time et) {
+        setStartTime(st);
+        setEndTime(et);
+        repeat = false;
+        sunday = false;
+        monday = false;
+        tuesday = false;
+        wednesday = false;
+        thursday = false;
+        friday = false;
+        saturday = false;
+    }
+
+    public boolean isInRange(Time t) {
+
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public Time getTimeRemaining() {
+
+    }
+
+    public void setStartTime(Time st) {
+        startTime = st;
+    }
+
+    public void setEndTime(Time et) {
+        endTime = et;
+    }
+
+    public void setRepeat(Boolean b) {
+        repeat = b;
+    }
+
+    public void setDays(ArrayList<String> al) {
+
+    }
 }
