7c17b7058da210b674447545965ab3045e25a9d2
Hunter Hurja <HunterHurja@Hunters-MacBook-Pro-2.local>
2017-10-06 15:04:39 -0700

    some work in model

--- Focus/app/src/main/java/Model/FocusModel.java
+++ Focus/app/src/main/java/Model/FocusModel.java
@@ -1,8 +1,54 @@
 package Model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by aaronrschrock on 10/6/17.
  */
 
 public class FocusModel {
+    private ArrayList<Schedule> schedules;
+    private ArrayList<Profile> profiles;
+    private ArrayList<App> apps;
+    private int numProfiles;
+    private int numSchedules;
+    private int numApps;
+
+    public FocusModel(){
+        numProfiles = 0;
+        numSchedules = 0;
+        numApps = 0;
+        schedules = new ArrayList<>();
+        profiles = new ArrayList<>();
+        apps = new ArrayList<>();
+
+    }
+
+    public void createNewProfile(String profileName){
+        profiles.add(new Profile(numProfiles, profileName));
+        numProfiles++;
+    }
+
+    public void removeProfile(int profileID){
+
+    }
+
+    public void createNewSchedule(String scheduleName){
+
+    }
+
+    public void removeSchedule(int scheduleID){
+
+    }
+
+    public void createNewApp(String appName){
+        apps.add(new App(numApps, appName));
+        numApps++;
+    }
+
+    public void removeApp(String appName){
+
+    };
+
 }
