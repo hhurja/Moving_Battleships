337f50203b30dcb84ba2eb8e5ac6e84808ba81e2
AdamMoffitt <admoffit@usc.edu>
2017-10-09 13:20:04 -0700

    blah

--- Focus/app/src/main/java/Controller/ProfileBlockingViewController.java
+++ Focus/app/src/main/java/Controller/ProfileBlockingViewController.java
@@ -1,12 +1,8 @@
 public class ProfileBlockingViewController {
-<<<<<<< HEAD
 
     // constructor
     public ProfileBlockingViewController() {
 
     }
 
-=======
- // test
->>>>>>> 0d99083cb67302574bac087897cb56f2bfb8a508
 }
