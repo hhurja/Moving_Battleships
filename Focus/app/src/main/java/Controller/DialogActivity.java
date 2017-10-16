dbba4894e5cae3c1c939c507ee8c5a6b867882e8
Shabina Rayan <srayan@usc.edu>
2017-10-16 13:57:54 -0700

    dialog changes

--- Focus/app/src/main/java/Controller/DialogActivity.java
+++ Focus/app/src/main/java/Controller/DialogActivity.java
@@ -15,46 +15,22 @@ import movingbattleship.org.focus.R;
 
 public class DialogActivity extends Activity implements OnClickListener {
 
-    Button ok_btn, cancel_btn;
+    Button ok_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
-
         ok_btn = (Button) findViewById(R.id.ok_btn_id);
-        cancel_btn = (Button) findViewById(R.id.cancel_btn_id);
-
         ok_btn.setOnClickListener(this);
-        cancel_btn.setOnClickListener(this);
-
     }
 
     @Override
-    public void onClick(View v) {
-
-        switch (v.getId()) {
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.ok_btn_id:
-
-                showToastMessage("Ok Button Clicked");
                 this.finish();
-
-                break;
-
-            case R.id.cancel_btn_id:
-
-                showToastMessage("Cancel Button Clicked");
-                this.finish();
-
                 break;
         }
-
     }
-
-    void showToastMessage(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT)
-                .show();
-    }
-
 }
