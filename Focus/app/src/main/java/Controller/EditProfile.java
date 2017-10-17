bd4bd4e6298b999df34f6ee07db90970ffa6054d
rlibowsky <libowsky@usc.edu>
2017-10-16 23:45:07 -0700

    jfiewo

--- Focus/app/src/main/java/Controller/EditProfile.java
+++ Focus/app/src/main/java/Controller/EditProfile.java
@@ -201,6 +201,7 @@ public class EditProfile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //focusModel.re
+                onBackPressed();
             }
         });
 
