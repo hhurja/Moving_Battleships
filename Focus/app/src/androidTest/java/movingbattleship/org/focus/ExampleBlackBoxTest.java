85691389e66a6f2b979de63c5338403ac7be4a5b
arschroc <arschroc@usc.edu>
2017-10-27 14:45:25 -0700

    Example black box

---
+++ Focus/app/src/androidTest/java/movingbattleship/org/focus/ExampleBlackBoxTest.java
@@ -0,0 +1,51 @@
+package movingbattleship.org.focus;
+
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.filters.LargeTest;
+
+import Controller.MainActivity;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import android.support.test.espresso.Espresso;
+
+/**
+ * Created by aaronrschrock on 10/27/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ExampleBlackBoxTest {
+
+    private String mStringToBetyped;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void changeText_sameActivity() throws InterruptedException {
+        Espresso.onView(withId(R.id.addProfileButton)).perform(click());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
