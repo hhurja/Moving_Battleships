8c89ec3c53c182fce49237e4609363c5271007ad
Shabina Rayan <srayan@usc.edu>
2017-10-27 15:49:30 -0700

    black box

--- Focus/app/src/androidTest/java/movingbattleship/org/focus/ExampleBlackBoxTest.java
+++ Focus/app/src/androidTest/java/movingbattleship/org/focus/ExampleBlackBoxTest.java
@@ -1,21 +1,31 @@
 package movingbattleship.org.focus;
 
 
+
+import static org.hamcrest.core.AllOf.allOf;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.matchers.JUnitMatchers.*;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.core.deps.guava.base.Strings;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.filters.LargeTest;
+import static android.support.test.espresso.Espresso.onView;
 
 import Controller.MainActivity;
 
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 import android.support.test.espresso.Espresso;
 
 /**
@@ -39,16 +49,27 @@ public class ExampleBlackBoxTest {
 
     @Test
     public void changeText_sameActivity() throws InterruptedException {
-
         //Click on add profile button
         Espresso.onView(withId(R.id.addProfileButton)).perform(click());
-
         //pause
         try {
-            Thread.sleep(10000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+    @Test
+    public void addSchedule() throws InterruptedException {
+        //Click on add profile button
+        Espresso.onView(withText("Schedules")).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.addScheduleButton)).perform(ViewActions.click());
+        Espresso.onView(allOf(withClassName(containsString("EditText")))).perform(replaceText("Another test"));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
